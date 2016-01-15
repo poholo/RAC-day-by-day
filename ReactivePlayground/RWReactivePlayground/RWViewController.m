a0cf1e216093899d3cdad3881bd62389cd13f7d1
mjc <mailjiancheng@163.com>
2016-01-15 14:32:27 +0800

    flatten map

--- ReactivePlayground/RWReactivePlayground/RWViewController.m
+++ ReactivePlayground/RWReactivePlayground/RWViewController.m
@@ -8,6 +8,7 @@
 
 #import "RWViewController.h"
 #import "RWDummySignInService.h"
+#import  <ReactiveCocoa.h>
 
 @interface RWViewController ()
 
@@ -16,8 +17,8 @@
 @property (weak, nonatomic) IBOutlet UIButton *signInButton;
 @property (weak, nonatomic) IBOutlet UILabel *signInFailureText;
 
-@property (nonatomic) BOOL passwordIsValid;
-@property (nonatomic) BOOL usernameIsValid;
+//@property (nonatomic) BOOL passwordIsValid;
+//@property (nonatomic) BOOL usernameIsValid;
 @property (strong, nonatomic) RWDummySignInService *signInService;
 
 @end
@@ -26,17 +27,142 @@
 
 - (void)viewDidLoad {
   [super viewDidLoad];
+    /*
+    [self.usernameTextField.rac_textSignal subscribeNext:^(id x) {
+        NSLog(@"%@",x);
+    }];
   
-  [self updateUIState];
-  
-  self.signInService = [RWDummySignInService new];
-  
-  // handle text changes for both text fields
-  [self.usernameTextField addTarget:self action:@selector(usernameTextFieldChanged) forControlEvents:UIControlEventEditingChanged];
-  [self.passwordTextField addTarget:self action:@selector(passwordTextFieldChanged) forControlEvents:UIControlEventEditingChanged];
-  
+    [self.passwordTextField.rac_textSignal subscribeNext:^(id x) {
+        NSLog(@"%@",x);
+    }];
+     */
+    
+    /*
+    [[[self.usernameTextField.rac_textSignal
+      map:^id(NSString* value) {
+          return @(value.length);
+      }]
+      filter:^BOOL(NSNumber *length) {
+        return [length integerValue] > 6;
+      }] subscribeNext:^(id x) {
+        NSLog(@"%@", x);
+      }];
+     */
+    
+    RACSignal *validUserSignal = [self.usernameTextField.rac_textSignal map:^id(NSString *text) {
+        return @([self isValidUsername:text]);
+    }];
+    
+    RACSignal *validPasswordSignal = [self.passwordTextField.rac_textSignal map:^id(NSString *text) {
+        return @([self isValidPassword:text]);
+    }];
+    
+    
+    /*
+    [[validPasswordSignal map:^id(NSNumber *passwordValid) {
+            return [passwordValid boolValue] ? [UIColor clearColor] : [UIColor yellowColor];
+        }]
+        subscribeNext:^(UIColor *bgColor) {
+            self.passwordTextField.backgroundColor = bgColor;
+        }];
+    
+    [[validUserSignal map:^id(NSNumber *usernameValid) {
+            return [usernameValid boolValue] ? [UIColor clearColor] : [UIColor orangeColor];
+        }] subscribeNext:^(UIColor *bgColor) {
+            self.usernameTextField.backgroundColor = bgColor;
+        }];
+     */
+    
+    // has better method
+    /*
+     从概念上来说，就是把之前信号的输出应用到输入框的backgroundColor属性上。但是上面的用法不是很好。
+     
+     幸运的是，ReactiveCocoa提供了一个宏来更好的完成上面的事情。把下面的代码直接加到viewDidLoad中两个信号的代码后面：
+     */
+    
+    /**
+        RAC宏允许直接把信号的输出应用到对象的属性上。RAC宏有两个参数，第一个是需要设置属性值的对象，第二个是属性名。每次信号产生一个next事件，传递过来的值都会应用到该属性上。
+     */
+    
+//    RAC(self.usernameTextField, backgroundColor) = [validUserSignal map:^id(NSNumber *usernameValid) {
+//        return  [usernameValid boolValue] ? [UIColor greenColor] : [UIColor purpleColor];
+//    }];
+//    
+//    RAC(self.passwordTextField, backgroundColor) = [validPasswordSignal map:^id(NSNumber *passwordValid) {
+//        return [passwordValid boolValue] ? [UIColor orangeColor] : [UIColor redColor];
+//    }];
+    
+    
+    /**
+     你是否好奇为什么要创建两个分开的validPasswordSignal和validUsernameSignal呢，而不是每个输入框一个单独的管道呢？（？）稍安勿躁，答案就在下面。
+     
+     原文：Are you wondering why you created separate validPasswordSignal and validUsernameSignal signals, as opposed to a single fluent pipeline for each text field? Patience dear reader, the method behind this madness will become clear shortly!
+     
+     聚合信号
+     
+     目前在应用中，登录按钮只有当用户名和密码输入框的输入都有效时才工作。现在要把这里改成响应式的。
+     
+     现在的代码中已经有可以产生用户名和密码输入框是否有效的信号了——validUsernameSignal和validPasswordSignal了。现在需要做的就是聚合这两个信号来决定登录按钮是否可用。
+     */
+    
+//    RACSignal * signUpActiveSignal = [RACSignal combineLatest:@[validUserSignal, validPasswordSignal] reduce:^id(NSNumber *usernameValid, NSNumber *passwordValid) {
+//        return @([usernameValid boolValue] && [passwordValid boolValue]);
+//    }];
+    
+    //每次这两个源信号的任何一个产生新值时，reduce block都会执行，block的返回值会发给下一个信号。
+//    [[self.signInButton rac_signalForControlEvents:UIControlEventTouchUpInside] subscribeNext:^(id x) {
+//        NSLog(@"btn click");
+//    }];
+
+
+    /*
+    [[[self.signInButton rac_signalForControlEvents:UIControlEventTouchUpInside] map:^id(id value) {
+            return [self signInSignal];
+        }]
+        subscribeNext:^(id x) {
+            NSLog(@"sigin %@", [x class]);
+        }];
+    
+     */
+    
+    
+    [[[self.signInButton rac_signalForControlEvents:UIControlEventTouchUpInside] flattenMap:^RACStream *(id value) {
+        return [self signInSignal];
+    }]
+     subscribeNext:^(NSNumber*signedIn){
+         BOOL success =[signedIn boolValue];
+         self.signInFailureText.hidden = success;
+         if(success){
+             [self performSegueWithIdentifier:@"signInSuccess" sender:self];
+         }
+     }];
+
+    /*
+    [[[self.signInButton
+       rac_signalForControlEvents:UIControlEventTouchUpInside]
+      flattenMap:^id(id x){
+          return [self signInSignal];
+      }]
+     subscribeNext:^(id x){
+         NSLog(@"Sign in result: %@", x);
+     }];
+     */
+
   // initially hide the failure message
   self.signInFailureText.hidden = YES;
+}
+
+- (RACSignal *)signInSignal {
+    return [RACSignal createSignal:^RACDisposable *(id subscriber){
+        [self.signInService
+         signInWithUsername:self.usernameTextField.text
+         password:self.passwordTextField.text
+         complete:^(BOOL success){
+             [subscriber sendNext:@(success)];
+             [subscriber sendCompleted];
+         }];
+        return nil;
+    }];
 }
 
 - (BOOL)isValidUsername:(NSString *)username {
@@ -44,43 +170,7 @@
 }
 
 - (BOOL)isValidPassword:(NSString *)password {
-  return password.length > 3;
-}
-
-- (IBAction)signInButtonTouched:(id)sender {
-  // disable all UI controls
-  self.signInButton.enabled = NO;
-  self.signInFailureText.hidden = YES;
-  
-  // sign in
-  [self.signInService signInWithUsername:self.usernameTextField.text
-                            password:self.passwordTextField.text
-                            complete:^(BOOL success) {
-                              self.signInButton.enabled = YES;
-                              self.signInFailureText.hidden = success;
-                              if (success) {
-                                [self performSegueWithIdentifier:@"signInSuccess" sender:self];
-                              }
-                            }];
-}
-
-    
-// updates the enabled state and style of the text fields based on whether the current username
-// and password combo is valid
-- (void)updateUIState {
-  self.usernameTextField.backgroundColor = self.usernameIsValid ? [UIColor clearColor] : [UIColor yellowColor];
-  self.passwordTextField.backgroundColor = self.passwordIsValid ? [UIColor clearColor] : [UIColor yellowColor];
-  self.signInButton.enabled = self.usernameIsValid && self.passwordIsValid;
-}
-
-- (void)usernameTextFieldChanged {
-  self.usernameIsValid = [self isValidUsername:self.usernameTextField.text];
-  [self updateUIState];
-}
-
-- (void)passwordTextFieldChanged {
-  self.passwordIsValid = [self isValidPassword:self.passwordTextField.text];
-  [self updateUIState];
+  return password.length > 5;
 }
 
 @end
